12a8360f2d213164f454535159995e6889f163b7
Estudiantes <Estudiantes@Labing70203.udistrital.edu.co>
2017-08-28 17:05:59 -0500

    Switch para las operaciones.

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,21 @@
+
+package calculadora;
+
+
+public class Calculadora {
+    
+    float operando1;
+    float operando2;
+    float resultado;
+    
+    void sumar(){
+        
+        resultado= operando1+operando2;
+    }
+    
+    void restar() {
+        
+        resultado=operando1-operando2;
+    }
+    
+}
